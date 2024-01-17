438ccb9ca917afdcad7f9498c5aa4806969dfa48
Δlex Santos <140655997+alexsantos-dev@users.noreply.github.com>
2024-01-17 12:05:10 -0300

    Testando metodo padrao de conexao ao mysql

--- src/database/db.js
+++ src/database/db.js
@@ -3,7 +3,13 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
-const pool = mysql.createPool(process.env.DB_URI);
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+});
 
 async function conectDatabase() {
     console.log("Aguardando conexao...")
