03b2bea77356b203015fcd4ef821d7a4aed199d8
Δlex Santos <140655997+alexsantos-dev@users.noreply.github.com>
2024-01-17 21:23:33 -0300

    refact pool in database

--- src/database/db.js
+++ src/database/db.js
@@ -3,13 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
-});
+const pool = mysql.createPool(process.env.DB_URI);
 
 async function conectDatabase() {
     console.log("Aguardando conexao...")
